7ebe595cf6f51c9b110d8796af2c147ec896ff37
Patrick Gonzales <101566290+PatrickGonzaless@users.noreply.github.com>
2022-07-18 21:07:25 -0300

    JS

--- js/script.js
+++ js/script.js
@@ -29,8 +29,86 @@ const pipe = document.querySelectorAll('.pipe')[0];
                 mario.style.marginLeft = "50px";
 
                 clearInterval(loop);
+
+                document.querySelector(".game-over").style.display="block";
+
+                stop();
+
             }
+            
 
         }, 10);
-        
-        document.addEventListener('keydown', jump);
+
+        function reiniciar(){
+
+            return document.location.reload()
+            
+
+            }   
+
+            function temporizador(cond){
+                let teste = 0;
+             
+                 setInterval(() => {
+                     if(cond == true) {
+                         teste += 1
+                         console.log(teste)
+                     }
+                 }, 1000)
+             };
+             
+            //  if(teste2 == true) {
+            //      temporizador();
+            //  }
+
+            window.addEventListener('load', () => start())
+
+            var sec=0
+            var min=0
+            var hr=0
+            
+            var interval
+            
+            function twoDigits(digit){
+                if(digit<10){
+                    return('0'+digit)
+                }else{
+                    return(digit)
+                }
+            }
+            
+            function start(){
+                watch()
+                interval= setInterval(watch,10)
+            
+            
+            }
+            
+            function pause(){
+                clearInterval(interval)
+            
+            }
+            
+            function stop(){
+                clearInterval(interval)
+                sec=0
+                min=0
+                window.alert("You've stopped at: "+document.getElementById('watch').innerText)
+                document.getElementById('watch').innerText='00:00:00'
+            
+            }
+            
+            function watch(){
+                sec++
+                if(sec==60){
+                    min++
+                    sec=0
+                    if(min==60){
+                        min=0
+                        hr++
+                    }
+                }
+                document.querySelector('.pontos').innerText=twoDigits(hr)+':'+twoDigits(min)+':'+twoDigits(sec)
+            }
+            
+        document.addEventListener('keydown' , jump);
